2fa1faf1a3317ef1dbacee9a60f545af2949119f
apelsina8 <112387975+apelsina8@users.noreply.github.com>
2025-03-11 21:39:30 +0300

    Update Main.java

--- src/main/java/ru/apelsina8/nordic/Main.java
+++ src/main/java/ru/apelsina8/nordic/Main.java
@@ -2,6 +2,6 @@ package ru.apelsina8.nordic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VersionControl vc = new VersionControl(10);
     }
 }
