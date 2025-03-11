27eb8a5cda9ece158d5d35dcb75b2fda1594fc61
apelsina8 <arhishow@gmaill.com>
2025-03-11 21:20:56 +0300

    Add a flag

--- src/main/java/ru/apelsina8/nordic/VersionControl.java
+++ src/main/java/ru/apelsina8/nordic/VersionControl.java
@@ -2,12 +2,19 @@ package ru.apelsina8.nordic;
 
 public class VersionControl {
     private final int version;
+    private final boolean flag;
 
     public VersionControl(int version) {
         this.version = version;
+        this.flag = false;
     }
 
     public int getVersion() {
         return version;
     }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
 }
